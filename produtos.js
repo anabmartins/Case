73e9f3d6e35ccd97e6346a90ad412d62f42a3794
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-04-22 14:41:05 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -1,18 +1,43 @@
 const verificarProcedencia = function(opcao){
     let procedencia = document.getElementById('procedencia')
+    let c1 = "Sul"
+    let c2 = "Norte"
+    let c3 = "Leste"
+    let c4 = "Oeste"
+    let c5 = "Nordeste"
+    let c6 = "Sudeste"
+    let c7 = "Centro-oeste"
+
     switch(opcao){
-        case 'uva':
-        procedencia.innerHTML = "Preço da uva é R$1,70"
+    case 'pneu':
+        procedencia.innerHTML = c1
         break
-    case 'laranja':
-        procedencia.innerHTML = "Preço da laranja é R$2,50"
+    case 'oleo':
+        procedencia.innerHTML = c5
         break
-    case 'maça':
-        procedencia.innerHTML = "Preço da maçã é R$3,09"
+    case 'alarme':
+        procedencia.innerHTML = c2
         break
-        case 'pera':
-            procedencia.innerHTML = "Preço da pera é R$4,05"
-            break
+    case 'capa':
+            procedencia.innerHTML = c7
+        break
+    case 'tapete':
+            procedencia.innerHTML = c6
+        break       
+        
+    case 'cinto':
+        procedencia.innerHTML = c3
+    break       
+
+    case 'lampada':
+            procedencia.innerHTML = c4
+        break       
+
+    case 'amortecedor':
+            procedencia.innerHTML = c7
+        break       
+
+            
     default:
         procedencia.innerHTML = "Resultado não existe"
             break
